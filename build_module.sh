1a23bf4bf52ddc6e50c96f161595be9123fb17e5
David Beniamine <david@beniamine.net>
2017-03-20 18:31:52 +0100

    GPL Headers

--- build_module.sh
+++ build_module.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright (C) 2017  Tetras Libre <Contact@Tetras-Libre.fr>
+# Author: Beniamine, David <David.Beniamine@Tetras-Libre.fr>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 if [ `whoami`  != "root" ]
 then
     echo "this script should be run as root"
